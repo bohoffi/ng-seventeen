80a9d33ac8d5fbaf20313ef83b5de2453efd8582
Sebastian Hoffmann <S.Hoffmann@dvz-mv.de>
2023-11-10 08:34:41 +0100

    build: add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withViewTransitions())],
+  providers: [provideRouter(routes, withViewTransitions()), provideAnimations()],
 };
